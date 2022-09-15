cd6f3c989a9173d9d961a56ad7d4d21b39deed6f
omarurregodev <omarurrego07@gmail.com>
2022-09-14 23:54:13 -0500

    work in progress

--- index.js
+++ index.js
@@ -18,3 +18,35 @@ const server = app.listen(PORT, () => {
 })
 
 app.use(express.static("public"));
+
+const productos = [];
+
+const routerProductos = express.Router();
+
+app.use('/productos', routerProductos);
+
+
+routerProductos.get("/", (req, res) => {
+    res.json({ productos });
+})
+
+routerProductos.get('/:id', (req, res) => {
+    const productId = productos.filter(data => data.id === req.params);
+    console.log(productId);
+    res.json({productId})
+})
+
+routerProductos.post("/", (req, res) => {
+    if (productos.length > 0) {
+        const idUltimo = productos.sort((a, b) => b.id - a.id)[0].id;
+        req.body.id = idUltimo + 1;
+        console.log(req.body);
+        productos.push(req.body);
+        res.json({"mensaje" : "se agrego el producto con ID: " + req.body.id})
+    } else {
+        req.body.id = 1;
+        console.log(req.body);
+        productos.push(req.body);
+        res.json({"mensaje" : "se agrego el producto con ID: " + req.body.id})
+    }
+})
